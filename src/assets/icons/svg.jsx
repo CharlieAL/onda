3d77d245b2f6981cc90827988b1e0aa06cd1e2a2
CharlieAl <charlie.lerma.ramirez@gmail.com>
2023-11-26 10:52:19 -0700

    add icons svg

---
+++ src/assets/icons/svg.jsx
@@ -0,0 +1,433 @@
+export const HeartIcon = () => (
+  <svg
+    className='icon icon-tabler icon-tabler-heart'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    ></path>
+    <path d='M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572'></path>
+  </svg>
+)
+
+export const UsersIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-users'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    ></path>
+    <path d='M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0'></path>
+    <path d='M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2'></path>
+    <path d='M16 3.13a4 4 0 0 1 0 7.75'></path>
+    <path d='M21 21v-2a4 4 0 0 0 -3 -3.85'></path>
+  </svg>
+)
+
+export const UserIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-user'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0' />
+    <path d='M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2' />
+  </svg>
+)
+
+export const HomeIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-home-2'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    ></path>
+    <path d='M5 12l-2 0l9 -9l9 9l-2 0'></path>
+    <path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7'></path>
+    <path d='M10 12h4v4h-4z'></path>
+  </svg>
+)
+
+export const LogoutIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-logout-2'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    ></path>
+    <path d='M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2'></path>
+    <path d='M15 12h-12l3 -3'></path>
+    <path d='M6 15l-3 -3'></path>
+  </svg>
+)
+
+export const PlusIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-plus'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    ></path>
+    <path d='M12 5l0 14'></path>
+    <path d='M5 12l14 0'></path>
+  </svg>
+)
+
+export const CompanyIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-settings-2'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    ></path>
+    <path d='M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z'></path>
+    <path d='M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0'></path>
+  </svg>
+)
+
+export const SendIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-send'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M10 14l11 -11' />
+    <path d='M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5' />
+  </svg>
+)
+
+export const ChatIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-brand-hipchat'
+    width={32}
+    height={32}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M17.802 17.292s.077 -.055 .2 -.149c1.843 -1.425 3 -3.49 3 -5.789c0 -4.286 -4.03 -7.764 -9 -7.764c-4.97 0 -9 3.478 -9 7.764c0 4.288 4.03 7.646 9 7.646c.424 0 1.12 -.028 2.088 -.084c1.262 .82 3.104 1.493 4.716 1.493c.499 0 .734 -.41 .414 -.828c-.486 -.596 -1.156 -1.551 -1.416 -2.29z' />
+    <path d='M7.5 13.5c2.5 2.5 6.5 2.5 9 0' />
+  </svg>
+)
+
+export const BackIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-chevron-left'
+    width={32}
+    height={32}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M15 6l-6 6l6 6' />
+  </svg>
+)
+
+export const UserPlusIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-user-plus'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0' />
+    <path d='M16 19h6' />
+    <path d='M19 16v6' />
+    <path d='M6 21v-2a4 4 0 0 1 4 -4h4' />
+  </svg>
+)
+
+export const ArrowRightIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-chevron-right'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M9 6l6 6l-6 6' />
+  </svg>
+)
+
+export const EmailIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-mail'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z' />
+    <path d='M3 7l9 6l9 -6' />
+  </svg>
+)
+
+export const SunIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-sun-filled'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path
+      d='M12 19a1 1 0 0 1 .993 .883l.007 .117v1a1 1 0 0 1 -1.993 .117l-.007 -.117v-1a1 1 0 0 1 1 -1z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M18.313 16.91l.094 .083l.7 .7a1 1 0 0 1 -1.32 1.497l-.094 -.083l-.7 -.7a1 1 0 0 1 1.218 -1.567l.102 .07z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M7.007 16.993a1 1 0 0 1 .083 1.32l-.083 .094l-.7 .7a1 1 0 0 1 -1.497 -1.32l.083 -.094l.7 -.7a1 1 0 0 1 1.414 0z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M4 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M21 11a1 1 0 0 1 .117 1.993l-.117 .007h-1a1 1 0 0 1 -.117 -1.993l.117 -.007h1z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M6.213 4.81l.094 .083l.7 .7a1 1 0 0 1 -1.32 1.497l-.094 -.083l-.7 -.7a1 1 0 0 1 1.217 -1.567l.102 .07z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M19.107 4.893a1 1 0 0 1 .083 1.32l-.083 .094l-.7 .7a1 1 0 0 1 -1.497 -1.32l.083 -.094l.7 -.7a1 1 0 0 1 1.414 0z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M12 2a1 1 0 0 1 .993 .883l.007 .117v1a1 1 0 0 1 -1.993 .117l-.007 -.117v-1a1 1 0 0 1 1 -1z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+    <path
+      d='M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z'
+      strokeWidth={0}
+      fill='currentColor'
+    />
+  </svg>
+)
+
+export const PhoneIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-phone-check'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2' />
+    <path d='M15 6l2 2l4 -4' />
+  </svg>
+)
+
+export const EditIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-pencil'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4' />
+    <path d='M13.5 6.5l4 4' />
+  </svg>
+)
+
+export const TicketIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='icon icon-tabler icon-tabler-ticket'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    strokeWidth={2}
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path
+      stroke='none'
+      d='M0 0h24v24H0z'
+      fill='none'
+    />
+    <path d='M15 5l0 2' />
+    <path d='M15 11l0 2' />
+    <path d='M15 17l0 2' />
+    <path d='M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2' />
+  </svg>
+)
