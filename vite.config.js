1c62e2fe77a378a4bc707f2e430971d4deebd6b1
CharlieAl <charlie.lerma.ramirez@gmail.com>
2023-11-26 23:09:51 -0700

    Update VitePWA plugin configuration

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => {
-              return url.pathname.startsWith('/api')
-            },
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'api-cache',
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })]
 })
