ea1aeb56739b4734a4895554ffc73ac7b633fedc
CharlieAl <charlie.lerma.ramirez@gmail.com>
2023-11-26 19:59:51 -0700

    Add skeleton loading for user cards

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -2,6 +2,7 @@
 import { useEffect } from 'react'
 import { useUserActions, useUserSelector } from '../hooks/useUsers'
 import CardUser from '../components/CardUser'
+import { Skeleton } from '@nextui-org/react'
 
 const Users = () => {
   const { fetchUsers } = useUserActions()
@@ -25,10 +26,21 @@ const Users = () => {
     <div>
       <ul>
         {users.map((user, i) => (
-          <CardUser
-            key={i}
-            {...user}
-          />
+          <div key={i}>
+            {loading ? (
+              <div className=' w-full flex items-center gap-3'>
+                <div>
+                  <Skeleton className='flex rounded-full w-12 h-12' />
+                </div>
+                <div className='w-full flex flex-col gap-2'>
+                  <Skeleton className='h-3 w-3/5 rounded-lg' />
+                  <Skeleton className='h-3 w-4/5 rounded-lg' />
+                </div>
+              </div>
+            ) : (
+              <CardUser {...user} />
+            )}
+          </div>
         ))}
       </ul>
     </div>
